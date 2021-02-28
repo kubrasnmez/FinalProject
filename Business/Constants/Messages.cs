d7ccafaa930e2a8102ff06843db640175e65c941
kubrasnmez <sonmezkbra@gmail.com>
2021-02-28 12:44:45 +0300

    14.gun

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -18,9 +18,9 @@ namespace Business.Constants
         public static string CategoryListed = "Kategoriler listelendi";
         public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
         public static string AuthorizationDenied = "Yetkiniz yok.";
-        public static string UserRegistered = "Kayıt olundu";
+        public static string UserRegistered = "Kayıt olundu.";
         public static string UserNotFound = "Kullanıcı bulunamadı.";
-        public static string PasswordError = "Şifre hatalı";
+        public static string PasswordError = "Şifre hatalı.";
         public static string SuccessfulLogin = "Giriş başarılı";
        public static string UserAlreadyExists = "Kullanıcı mevcut";
         public static string AccessTokenCreated = "Token oluşturuldu";
